d1b503efbbb3f4d8798813af3800c576e9e3b57a
phil <phil.c83@laposte.net>
2023-03-04 17:53:37 +0100

    maj

--- octave/Jupiter/Tx/Field/test_field.m
+++ octave/Jupiter/Tx/Field/test_field.m
@@ -4,8 +4,8 @@ clear all;
 #[s1,s2,s3]=fields(c1,c2,c3,r,theta)
 # tripolaire cable
 c1 = 1/2*exp(i*pi/3);
-c2 = 1/2;
-c3 = 1/2*exp(i*5*pi/3);
+c2 = 1/2*exp(i*5*pi/3);
+c3 = 1/2;
 theta = 0:pi/20:2*pi;
 [s1,s2,s3]=fields(c1,c2,c3,1,theta);
 figure();
@@ -17,9 +17,8 @@ plot(theta,s3,";s3;");
 
 # tetrapolaire cable
 c1 = 1/2*exp(i*5*pi/18);
-c1 = 1/2*exp(i*15*pi/18);
-c3 = 1/2*exp(i*25*pi/18);
-
+c2 = 1/2*exp(i*25*pi/18);
+c3 = 1/2*exp(i*15*pi/18);
 [s1,s2,s3]=fields(c1,c2,c3,1,theta);
 figure();
 plot(theta,s1,";s1;");
